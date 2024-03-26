b980eb7068f3d2072dae9e3b6ab4aed5268297c5
JacobT <jacobfung04@gmail.com>
2024-03-26 10:29:57 +0800

    lesson2 classwork

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+  public static void main(String[] args) {
+    //Start point of the program Hello World
+    //Tool: System.out.println()
+    System.out.println("Hello World, I am Jacob Tang");
+    System.out.println("Hello World, I am Jacob");
+  }
+}
