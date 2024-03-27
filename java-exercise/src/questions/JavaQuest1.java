ab249f7171cb5f7d6bde53e6890f3651966b2a4f
JacobT <jacobfung04@gmail.com>
2024-03-27 12:08:26 +0800

    lesson3 am CW

--- java-exercise/src/questions/JavaQuest1.java
+++ java-exercise/src/questions/JavaQuest1.java
@@ -24,28 +24,32 @@ public class JavaQuest1 {
     int first = 31;
     // declare the second one
     // code here ...
+    int second =42;
 
     System.out.println("The first one: " + first);
     System.out.println("The second one: " + second);
 
     // Add two numbers and print it out
     // code here ...
+    int sum = first + second;
     System.out.println("The sum is " + sum);
 
     // subtraction
     // code here with tips, pls fix ...
-    // subtract = first - second
+    int subtract = second - first;
     System.out.println("The subtraction reuslt is " + subtract);
 
     // another sum
     byte b1 = 60;
     byte b2 = 70;
     // code here with tips, pls fix ...
-    // sum = b1 + b2;
-    System.out.println("The another sum reuslt is " + sum);
+    int sum2 = b1 + b2;
+    System.out.println("The another sum reuslt is " + sum2);
 
     // declare two boolean variable with true and false value
     // code here
+    boolean varB = false;
+    boolean varC = true;
     System.out.println("The variable varB is " + varB);
     System.out.println("The variable varC is " + varC);
 
@@ -60,10 +64,11 @@ public class JavaQuest1 {
     int a = 7;
     int b = 3;
     // Fix it ...
-    int reminder = a - b;
+    int reminder = a % b;
     System.out.println("The variable reminder is " + reminder);
 
     // declare a variable x and assign value to it.
+    int x = 2;
     int e = 7;
     int o = 8;
     int y = 4;
