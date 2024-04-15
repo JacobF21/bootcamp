87d6ec9d61c71a948f2584bac2d423f921761d6a
JacobT <jacobfung04@gmail.com>
2024-04-15 17:43:52 +0800

    Week4

---
+++ java-by-topics/src/questions/java-6-class-constructor/Exercise10.java
@@ -0,0 +1,24 @@
+/**
+ * Expected Output: 8
+ */
+public class Exercise10 {
+    int x; // Create an instance attribute
+
+    // Create a class constructor for the Main class
+    public Exercise10() {
+        // Set the value for the instance attribute x to expected output
+        this.x =8;
+    }
+
+    public int getX(){
+        return this.x;
+    }
+
+    public static void main(String[] args) throws Exception {
+        // Create an instance of this class Exercise10
+        Exercise10 exercise10 = new Exercise10();
+        // Print the value of instance attribute
+        System.out.println(exercise10.getX());
+
+    }
+}
